1ed055da5ad9026b34a4a24ecf26ea6dce2d8e80
hadi kheyruri <hadi.kheyruri@beyondtherack.com>
2015-01-02 16:08:00 -0500

    better (flexible and dynamic) UX/ UI architecture for checkout

---
+++ BeyondTheRack/viewcontroller/PaymentCheckoutViewController.h
@@ -0,0 +1,13 @@
+//
+//  PaymentCheckoutViewController.h
+//  BeyondTheRack
+//
+//  Created by Hadi Kheyruri on 2015-01-02.
+//  Copyright (c) 2015 Hadi Kheyruri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PaymentCheckoutViewController : UIViewController
+
+@end
