5c115f38f1a9ba1fd4ed6a23f641e71b973ffee7
hadi kheyruri <hadi.kheyruri@beyondtherack.com>
2015-01-08 09:57:44 -0500

    commit three files

---
+++ BeyondTheRack/view/EventTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  EventTableViewCell.h
+//  BeyondTheRack
+//
+//  Created by Hadi Kheyruri on 2015-01-08.
+//  Copyright (c) 2015 Hadi Kheyruri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventTableViewCell : UITableViewCell
+
+@end
---
+++ BeyondTheRack/view/EventTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  EventTableViewCell.m
+//  BeyondTheRack
+//
+//  Created by Hadi Kheyruri on 2015-01-08.
+//  Copyright (c) 2015 Hadi Kheyruri. All rights reserved.
+//
+
+#import "EventTableViewCell.h"
+
+@implementation EventTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
