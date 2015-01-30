2834419e59bed20b376b353689a9d71af0fdd11c
hadi kheyruri <hadi.kheyruri@beyondtherack.com>
2015-01-30 12:42:00 -0500

    new slider on its way

---
+++ BeyondTheRack/viewcontroller/BTRSearchFilterTVC.h
@@ -0,0 +1,13 @@
+//
+//  BTRSearchFilterTVC.h
+//  BeyondTheRack
+//
+//  Created by Hadi Kheyruri on 2015-01-30.
+//  Copyright (c) 2015 Hadi Kheyruri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BTRSearchFilterTVC : UITableViewController
+
+@end
