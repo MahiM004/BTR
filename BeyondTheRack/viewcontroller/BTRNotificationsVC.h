0e33f5244f838e3b275ad50df4eedc00a1d66d10
Hadi Kheyruri <hadi.kheyruri@ITs-Mac-mini.local>
2015-06-02 15:37:42 -0400

    just to commit

--- BeyondTheRack/viewcontroller/BTRNotificationsVC.h
+++ BeyondTheRack/viewcontroller/BTRNotificationsVC.h
@@ -10,6 +10,18 @@
 #import "TNRadioButtonGroup.h"
 #import "User+AppServer.h"
 
+
+enum btrEmailNotificationFrequency
+{
+    btrAllEmails, // == 0 (by default)
+    btrDailyEmails, // == 1 (incremented by 1 from previous)
+    btrThreeTimesAWeekEmails, // == 2
+    btrWeeklyEmails,
+    btrNoEmails
+};
+
+
+
 @interface BTRNotificationsVC : UIViewController
 
 @property (weak, nonatomic) IBOutlet UIView *radioView;
--- BeyondTheRack/viewcontroller/BTRNotificationsVC.m
+++ BeyondTheRack/viewcontroller/BTRNotificationsVC.m
@@ -10,29 +10,65 @@
 
 @interface BTRNotificationsVC ()
 
+@property (nonatomic, assign) BOOL pushNotifications;
+@property (nonatomic, assign) BOOL womenreminders;
+@property (nonatomic, assign) BOOL mensreminders;
+@property (nonatomic, assign) BOOL childreminders;
+@property (nonatomic, assign) BOOL homereminders;
+@property (nonatomic, assign) enum btrEmailNotificationFrequency emailFrequency;
+
 @end
 
 @implementation BTRNotificationsVC
 
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
 
-    
-    //[self createHorizontalList];
+    [self setupPreferencesListAttributesforList:[[self user] preferencesList]];
     [self createVerticalList];
-    //[self createHorizontalListWithImage];
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 
- 
+- (void)setupPreferencesListAttributesforList:(NSString *)preferencesList {
+    
+    if (![self user])
+        return;
+    
+    if ([preferencesList containsString:@"partners"])
+        [[self pushNotificationSwitch] setOn:YES];
+    
+    if ([preferencesList containsString:@"womenreminders"])
+        [[self womenSwitch] setOn:YES];
+    
+    if ([preferencesList containsString:@"mensreminders"])
+        [[self menSwitch] setOn:YES];
+          
+    if ([preferencesList containsString:@"childreminders"])
+        [[self childrenSwitch] setOn:YES];
+    
+    if ([preferencesList containsString:@"homereminders"])
+        [[self homeSwitch] setOn:YES];
+    
+    if ([preferencesList containsString:@"dailyreminders"]) {
+        [self setEmailFrequency:btrAllEmails];
+    } else if ([preferencesList containsString:@"oncedailyreminders"]) {
+        [self setEmailFrequency:btrDailyEmails];
+    } else if ([preferencesList containsString:@"threetimesweeklyreminders"]) {
+        [self setEmailFrequency:btrThreeTimesAWeekEmails];
+    } else if ([preferencesList containsString:@"weeklyreminders"]) {
+        [self setEmailFrequency:btrWeeklyEmails];
+    } else {
+        [self setEmailFrequency:btrNoEmails];
+    }
+    
+}
+
+
 
 - (void)createVerticalList {
+    
     TNRectangularRadioButtonData *allEmailsData = [TNRectangularRadioButtonData new];
     allEmailsData.labelText = @"ALL";
     allEmailsData.identifier = @"all";
@@ -59,7 +95,6 @@
     threeTimesData.borderColor = [UIColor blackColor];
     threeTimesData.rectangleColor = [UIColor blackColor];
     
-    
     TNRectangularRadioButtonData *weeklyData = [TNRectangularRadioButtonData new];
     weeklyData.labelText = @"WEEKLY";
     weeklyData.identifier = @"weekly";
@@ -71,7 +106,6 @@
     weeklyData.borderColor = [UIColor blackColor];
     weeklyData.rectangleColor = [UIColor blackColor];
     
-
     TNRectangularRadioButtonData *noneData = [TNRectangularRadioButtonData new];
     noneData.labelText = @"NONE";
     noneData.identifier = @"none";
@@ -83,7 +117,6 @@
     noneData.borderColor = [UIColor blackColor];
     noneData.rectangleColor = [UIColor blackColor];
     
-    
     self.emailNotificationGroup = [[TNRadioButtonGroup alloc] initWithRadioButtonData:@[allEmailsData, onceDayData, threeTimesData, weeklyData, noneData] layout:TNRadioButtonGroupLayoutVertical];
     self.emailNotificationGroup.identifier = @"email group";
     [self.emailNotificationGroup create];
@@ -93,11 +126,9 @@
     
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(emailGroupUpdated:) name:SELECTED_RADIO_BUTTON_CHANGED object:self.emailNotificationGroup];
     
-
-    
     [self.emailNotificationGroup update];
-    
 }
+
 
 
 - (void)emailGroupUpdated:(NSNotification *)notification {
@@ -105,18 +136,50 @@
 }
 
 
-
 - (void)dealloc {
+    
      [[NSNotificationCenter defaultCenter] removeObserver:self name:SELECTED_RADIO_BUTTON_CHANGED object:self.emailNotificationGroup];
- }
-
-
+}
 
 
 - (IBAction)updatedTapped:(UIButton *)sender {
+    
 }
 
 
 
 
+
+
+
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
