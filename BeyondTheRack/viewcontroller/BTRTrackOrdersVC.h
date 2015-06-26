cffe8c13dbe839530454f9fd9a6b643cd90aed93
Hadi Kheyruri <hadi.kheyruri@ITs-Mac-mini.local>
2015-06-26 10:48:53 -0400

    added track orders VC

---
+++ BeyondTheRack/viewcontroller/BTRTrackOrdersVC.h
@@ -0,0 +1,13 @@
+//
+//  BTRTrackOrdersVC.h
+//  BeyondTheRack
+//
+//  Created by Hadi Kheyruri on 2015-06-26.
+//  Copyright (c) 2015 Hadi Kheyruri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BTRTrackOrdersVC : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+@end
---
+++ BeyondTheRack/viewcontroller/BTRTrackOrdersVC.m
@@ -0,0 +1,59 @@
+//
+//  BTRTrackOrdersVC.m
+//  BeyondTheRack
+//
+//  Created by Hadi Kheyruri on 2015-06-26.
+//  Copyright (c) 2015 Hadi Kheyruri. All rights reserved.
+//
+
+#import "BTRTrackOrdersVC.h"
+
+
+@interface BTRTrackOrdersVC ()
+
+@end
+
+@implementation BTRTrackOrdersVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+#pragma mark - Table view data source
+
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 161;
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"ItemOrderCellIdentifier";
+    UITableViewCell *cell;// = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
+
+
+    return cell;
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
