460809f1f17728bf958d98afeff18a4181f15e3d
Hadi Kheyruri <hadi.kheyruri@ITs-Mac-mini.local>
2015-07-16 12:02:06 -0400

    just to commit

--- BeyondTheRack/view/BTRBagTableViewCell.m
+++ BeyondTheRack/view/BTRBagTableViewCell.m
@@ -36,8 +36,8 @@
 
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    
     [super setSelected:selected animated:animated];
-
 }
 
 
@@ -60,3 +60,25 @@
 
 
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
