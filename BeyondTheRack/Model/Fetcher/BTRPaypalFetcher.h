3741ee9824bf85fe6df57e401390e6f9727e749c
Ali Pourhadi <ali.pourhadi@MTLLMC0629.local>
2015-07-27 15:28:38 -0400

    adding Paypal fetcher class

---
+++ BeyondTheRack/Model/Fetcher/BTRPaypalFetcher.h
@@ -0,0 +1,14 @@
+//
+//  BTRPaypalFetcher.h
+//  BeyondTheRack
+//
+//  Created by Ali Pourhadi on 2015-07-27.
+//  Copyright (c) 2015 Hadi Kheyruri. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "BTRFAQFetcher.h"
+
+@interface BTRPaypalFetcher : BTRFetcher
+
+@end
---
+++ BeyondTheRack/Model/Fetcher/BTRPaypalFetcher.m
@@ -0,0 +1,33 @@
+//
+//  BTRPaypalFetcher.m
+//  BeyondTheRack
+//
+//  Created by Ali Pourhadi on 2015-07-27.
+//  Copyright (c) 2015 Hadi Kheyruri. All rights reserved.
+//
+
+#import "BTRPaypalFetcher.h"
+
+@implementation BTRPaypalFetcher
+
++ (NSURL *)URLforStartPaypal
+{
+    return [self URLForQuery:[NSString stringWithFormat:@"%@/checkout/paypal", BASEURL]];
+}
+
++ (NSURL *)URLforPaypalInfo
+{
+    return [self URLForQuery:[NSString stringWithFormat:@"%@/checkout/info/paypal", BASEURL]];
+}
+
++ (NSURL *)URLforPaypalProcess
+{
+    return [self URLForQuery:[NSString stringWithFormat:@"%@/checkout/process/paypal", BASEURL]];
+}
+
++ (NSURL *)URLforPaypalProcessCallBackWithTransactionNumber:(NSString *)transaction
+{
+    return [self URLForQuery:[NSString stringWithFormat:@"%@/checkout/process/paypal/%@", BASEURL,transaction]];
+}
+
+@end
