d804c51b8ec8f7e49eebb5674ab22bc3bfe081a9
Ali Pourhadi <ali.pourhadi@MTLLMC0629.local>
2015-07-29 14:22:58 -0400

    adding BRTConfirmation View Controller

---
+++ BeyondTheRack/viewcontroller/BTRConfirmationViewController.h
@@ -0,0 +1,13 @@
+//
+//  BTRConfirmationViewController.h
+//  BeyondTheRack
+//
+//  Created by Ali Pourhadi on 2015-07-29.
+//  Copyright (c) 2015 Hadi Kheyruri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BTRConfirmationViewController : UIViewController
+
+@end
