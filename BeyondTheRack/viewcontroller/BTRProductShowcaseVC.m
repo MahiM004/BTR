9f9b974c642db0a7d65fb3200193ea7c7f21fa73
Ali Pourhadi <ali.pourhadi@MTLLMC0629.local>
2015-08-27 12:14:46 -0400

    adding sort feature for items in event

--- BeyondTheRack/viewcontroller/BTRProductShowcaseVC.m
+++ BeyondTheRack/viewcontroller/BTRProductShowcaseVC.m
@@ -18,6 +18,8 @@
 
 #define SIZE_NOT_SELECTED_STRING @"Select Size"
 
+#define SIZE_PICKER     1
+#define SORT_PICKER      2
 
 @interface BTRProductShowcaseVC ()
 
@@ -27,18 +29,46 @@
 @property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
 @property (weak, nonatomic) IBOutlet UILabel *eventTitleLabel;
 @property (weak, nonatomic) IBOutlet UIButton *bagButton;
-@property (strong, nonatomic) NSMutableArray *itemArray;
+@property (strong, nonatomic) NSMutableArray *originalItemArray;
 @property (copy, nonatomic) NSMutableArray *variantInventoriesArray; // an Array of variantInventory Dictionaries
 @property (copy, nonatomic) NSMutableArray *attributesArray; // an Array of variantInventory Dictionaries
+
 @property (strong, nonatomic) NSMutableArray *chosenSizesArray;
 @property (assign, nonatomic) NSUInteger selectedCellIndexRow;
 @property (strong, nonatomic) NSMutableArray *bagItemsArray;
+
+
+// sort and filter
+@property (strong, nonatomic) NSArray* collectionViewResourceArray;
+@property (strong, nonatomic) NSArray* sortedItemsArray;
+
+@property (strong, nonatomic) NSArray* sizeArray;
+@property (strong, nonatomic) NSArray* sortArray;
+
+// textFields
+
+@property (weak, nonatomic) IBOutlet UITextField *filterSizeTextField;
+@property (weak, nonatomic) IBOutlet UITextField *sortTextField;
+
+
+// picker
+
+@property (nonatomic) NSUInteger pickerType;
+
+@property (weak, nonatomic) IBOutlet UIView *pickerParentView;
+@property (weak, nonatomic) IBOutlet UIPickerView *pickerView;
 
 @end
 
 
 @implementation BTRProductShowcaseVC
 
+
+- (NSArray *)collectionViewResourceArray {
+    if ([self.sortTextField.text isEqualToString:@"Suggested"])
+        return self.originalItemArray;
+    return self.sortedItemsArray;
+}
 
 - (NSMutableArray *)bagItemsArray {
     
@@ -69,12 +99,15 @@
 }
 
 
-- (NSMutableArray *)itemArray {
-    
-    if (!_itemArray) _itemArray = [[NSMutableArray alloc] init];
-    return _itemArray;
+- (NSMutableArray *)originalItemArray {
+    if (!_originalItemArray) _originalItemArray = [[NSMutableArray alloc] init];
+    return _originalItemArray;
 }
 
+- (NSArray *)sortArray {
+    if (!_sortArray) _sortArray = [[NSArray alloc]initWithObjects:@"Suggested",@"Discount Increasing",@"Discount Decreasing",@"Price Increasing",@"Price Decreasing",@"Type", nil];
+    return _sortArray;
+}
 
 
 - (void)viewWillAppear:(BOOL)animated
@@ -144,12 +177,12 @@
              [self.attributesArray addObject:itemDic[@"attributes"]];
          }
          
-         self.itemArray = [Item loadItemsfromAppServerArray:entitiesPropertyList withEventId:[self eventSku] forItemsArray:[self itemArray]];
+         self.originalItemArray = [Item loadItemsfromAppServerArray:entitiesPropertyList withEventId:[self eventSku] forItemsArray:[self originalItemArray]];
          
-         for (int i = 0; i < [self.itemArray count]; i++)
+         for (int i = 0; i < [self.originalItemArray count]; i++)
              [self.chosenSizesArray addObject:[NSNumber numberWithInt:-1]];
          
-         success([self itemArray]);
+         success([self originalItemArray]);
          
      } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
          
@@ -230,7 +263,7 @@
 
 - (NSInteger)collectionView:(UICollectionView *)view numberOfItemsInSection:(NSInteger)section {
     
-    return [self.itemArray count];
+    return [self.collectionViewResourceArray count];
 }
 
 
@@ -244,7 +277,7 @@
                                              toSizeCodesArray:[cell sizeCodesArray]
                                           toSizeQuantityArray:[cell sizeQuantityArray]];
 
-    Item *productItem = [self.itemArray objectAtIndex:indexPath.row];
+    Item *productItem = [self.collectionViewResourceArray objectAtIndex:indexPath.row];
     cell = [self configureViewForShowcaseCollectionCell:cell withItem:productItem andBTRSizeMode:sizeMode forIndexPath:indexPath];
 
     NSMutableArray *tempSizesArray = [cell sizesArray];
@@ -351,7 +384,7 @@
 
 -(void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath  {
     
-    Item *productItem = [self.itemArray objectAtIndex:indexPath.row];
+    Item *productItem = [self.originalItemArray objectAtIndex:indexPath.row];
     [self setSelectedIndexPath:indexPath];
     [self setSelectedBrandString:[productItem brand]];
     [self performSegueWithIdentifier:@"ProductDetailSegueIdentifier" sender:self];
@@ -375,7 +408,7 @@
         
         BTRProductDetailViewController *productDetailVC = [segue destinationViewController];
         productDetailVC.originVCString = EVENT_SCENE;
-        productDetailVC.productItem = [self.itemArray objectAtIndex:[self.selectedIndexPath row]];
+        productDetailVC.productItem = [self.originalItemArray objectAtIndex:[self.selectedIndexPath row]];
         productDetailVC.eventId = [self eventSku];
         productDetailVC.variantInventoryDictionary = [self.variantInventoriesArray objectAtIndex:[self.selectedIndexPath row]];
         productDetailVC.attributesDictionary = [self.attributesArray objectAtIndex:[self.selectedIndexPath row]];
@@ -389,8 +422,108 @@
 }
 
 
+#pragma mark - Filter & Suggestion
+
+- (IBAction)pickerParentTapped:(id)sender {
+    [self.pickerParentView setHidden:YES];
+}
+
+- (IBAction)sizeSelectionTapped:(id)sender {
+    [self loadPickerViewforType:SIZE_PICKER];
+}
+
+- (IBAction)sortSelectionTapped:(id)sender {
+    [self loadPickerViewforType:SORT_PICKER];
+}
 
 
+#pragma mark PickerView
+
+- (void)loadPickerViewforType:(NSUInteger)type {
+    [self setPickerType:type];
+    [self.pickerView reloadAllComponents];
+    [self.pickerParentView setHidden:FALSE];
+    [self.pickerView becomeFirstResponder];
+}
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return 1;
+}
+
+- (CGFloat)pickerView:(UIPickerView *)pickerView widthForComponent:(NSInteger)component {
+    return 300.0;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    if (self.pickerType == SIZE_PICKER)
+        return [self.sizeArray count];
+    if (self.pickerType == SORT_PICKER)
+        return [self.sortArray count];
+    return 0;
+}
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow: (NSInteger)row inComponent:(NSInteger)component {
+    
+    [self.pickerParentView setHidden:TRUE];
+    
+    if ([self pickerType] == SORT_PICKER) {
+        
+        
+        if ([self.sortTextField.text isEqualToString:[self.sortArray objectAtIndex:row]])
+            return; // we dont need reload collectionView
+        
+        [self.sortTextField setText:[self.sortArray objectAtIndex:row]];
+        
+        NSSortDescriptor *sortDescriptor;
+        switch (row) {
+            case 0:
+                break;
+            case 1:
+                sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"discount" ascending:YES];
+                break;
+            case 2:
+                sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"discount" ascending:NO];
+                break;
+            case 3:
+                sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"salePrice" ascending:YES];
+                break;
+            case 4:
+                sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"salePrice" ascending:NO];
+                break;
+            case 5:
+                sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"sku" ascending:YES];
+                break;
+            default:
+                break;
+        }
+        
+        
+        
+        
+        NSArray *sortDescriptors = [NSArray arrayWithObject:sortDescriptor];
+        self.sortedItemsArray = [self.originalItemArray sortedArrayUsingDescriptors:sortDescriptors];
+    }
+    
+    if ([self pickerType] == SIZE_PICKER) {
+        [self.filterSizeTextField setText:[self.sizeArray objectAtIndex:row]];
+    }
+    
+    [self.collectionView reloadData];
+}
+
+- (UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row forComponent:(NSInteger)component reusingView:(UIView *)view{
+    UILabel* tView = (UILabel*)view;
+    if (!tView){
+        tView = [[UILabel alloc] init];
+        tView.adjustsFontSizeToFitWidth = YES;
+        tView.textAlignment = NSTextAlignmentCenter;
+    }
+    if (self.pickerType == SORT_PICKER)
+        [tView setText:[self.sortArray objectAtIndex:row]];
+    if (self.pickerType == SIZE_PICKER)
+        [tView setText:[self.sizeArray objectAtIndex:row]];
+    return tView;
+}
 
 #pragma mark - BTRSelectSizeVC Delegate
 
