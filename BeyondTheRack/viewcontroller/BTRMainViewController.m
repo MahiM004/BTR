5f7837abe4e89c6a87b4f922c3607b93e25be1fb
Ali Pourhadi <ali.pourhadi@MTLLMC0629.local>
2015-09-02 11:15:30 -0400

    removing spaces

--- BeyondTheRack/viewcontroller/BTRMainViewController.m
+++ BeyondTheRack/viewcontroller/BTRMainViewController.m
@@ -22,49 +22,35 @@
 
 @interface BTRMainViewController ()
 
-
 @property (strong, nonatomic) TTScrollSlidingPagesController *slider;
 @property (weak, nonatomic) IBOutlet UIView *headerView;
 @property (weak, nonatomic) IBOutlet UIButton *bagButton;
-
 
 @end
 
 @implementation BTRMainViewController
 
-
-
-- (void)viewWillAppear:(BOOL)animated
-{
+- (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     [self getCartCountServerCallWithSuccess:^(NSString *bagCountString) {
         self.bagButton.badgeValue = bagCountString;
         
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
     }];
-    
     BTRFacetsHandler *sharedFacetHandler = [BTRFacetsHandler sharedFacetHandler];
     [sharedFacetHandler resetFacets];
     sharedFacetHandler.searchString = @"";
 }
 
-
 - (void)viewDidLayoutSubviews {
-    
     [super viewDidLayoutSubviews];
 }
 
-
 - (void)viewDidLoad {
-    
     [super viewDidLoad];
-    
     self.view.backgroundColor = [BTRViewUtility BTRBlack];
     self.headerView.backgroundColor = [BTRViewUtility BTRBlack];
 }
-
-
- 
 
 #pragma mark - Get bag count
 
@@ -88,7 +74,6 @@
 
 
 - (IBAction)searchButtonTapped:(UIButton *)sender {
-    
     UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
     BTRSearchViewController *viewController = (BTRSearchViewController *)[storyboard instantiateViewControllerWithIdentifier:@"SearchNavigationControllerIdentifier"];
     [self presentViewController:viewController animated:NO completion:nil];
@@ -96,22 +81,15 @@
 
 
 - (IBAction)tappedShoppingBag:(id)sender {
-
     UIStoryboard *storyboard = self.storyboard;
     UIViewController * vc = [storyboard instantiateViewControllerWithIdentifier:@"ShoppingBagViewController"];
     [self presentViewController:vc animated:YES completion:nil];
-    
 }
 
-
-
-- (NSInteger) modulaForIndex:(NSInteger)inputInt withCategoryCount:(NSInteger)count
-{
+- (NSInteger) modulaForIndex:(NSInteger)inputInt withCategoryCount:(NSInteger)count {
     NSInteger relevantInt = (inputInt >= 0) ? (inputInt % count) : ((inputInt % count) + count);
     return relevantInt;
 }
-
-
 
 - (IBAction)unwindFromShoppingBagToEventsScene:(UIStoryboardSegue *)unwindSegue {
 }
@@ -127,7 +105,6 @@
 
 - (IBAction)unwindToEventViewController:(UIStoryboardSegue *)unwindSegue {
 }
-
 
 @end
 
