5c97440441f969304b875745419dba495fc29490
Mahesh_iOS <mahesh.m@mca140.com>
2015-09-04 17:22:30 +0530

    Forgot Password is Fixed and Loader is added when request made

--- BeyondTheRack/viewcontroller/BTRForgotPasswordVC.m
+++ BeyondTheRack/viewcontroller/BTRForgotPasswordVC.m
@@ -11,7 +11,9 @@
 #import "BTRConnectionHelper.h"
 
 @interface BTRForgotPasswordVC ()
-
+{
+    NSDictionary * responseDic;
+}
 @end
 
 @implementation BTRForgotPasswordVC
@@ -31,15 +33,33 @@
 
 
 - (IBAction)newPasswordTapped:(UIButton *)sender {
-    if ([self.emailField.text length] > 0) {
-        [self resetPasswordforEmail:[[self emailField] text] success:^(NSString *didSucceed) {
-            [self alertUserforNewPassword];
-        } failure:^(NSError *error) {
-            // TODO: Check for internet connectivity!
-        }];
+    if ([self.emailField.text length] > 0 && [self validateEmailWithString:_emailField.text]) {
+        
+        [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+        dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
+            [self resetPasswordforEmail:[[self emailField] text] success:^(NSString *didSucceed) {
+                
+                int successUser = [responseDic[@"success"] intValue];
+                NSString * messege = responseDic[@"message"];
+                if (successUser == 1) {
+                    [self dismissKeyboard];
+                    [self showAlert:@"Check your email" msg:messege];
+                    [self performSegueWithIdentifier:@"unwindToLoginScene" sender:self];
+                    [self hideHUD];
+                } else {
+                    [self showAlert:@"Failed" msg:messege];
+                    [self hideHUD];
+                }
+                
+            } failure:^(NSError *error) {
+                [self hideHUD];
+            }];
+        });
+    } else {
+        [self showAlert:@"Failed" msg:@"It seems to be you did not given valid email , Please check Once (or) Check the Active Internet Connection"];
+        [self hideHUD];
     }
 }
-
 
 - (void)resetPasswordforEmail:(NSString *)emailString
                            success:(void (^)(id  responseObject)) success
@@ -47,35 +67,32 @@
     NSString *url = [NSString stringWithFormat:@"%@",[BTRUserFetcher URLforPasswordReset]];
     NSDictionary *params = (@{ @"email": emailString });
     [BTRConnectionHelper postDataToURL:url withParameters:params setSessionInHeader:NO success:^(NSDictionary *response) {
+        responseDic = [NSDictionary dictionaryWithDictionary:response];
         success(@"TRUE");
     } faild:^(NSError *error) {
         failure(error);
+        [self hideHUD];
     }];
 }
 
 
-- (void)alertUserforNewPassword {
-    [self dismissKeyboard];
-    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Check your email"
-                                                    message:@"A new password was sent to your email address!"
-                                                   delegate:self
-                                          cancelButtonTitle:nil
-                                          otherButtonTitles:@"Ok", nil];
-    [self performSegueWithIdentifier:@"unwindToLoginScene" sender:self];
-    [alert show];
+-(void)hideHUD {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [MBProgressHUD hideHUDForView:self.view animated:YES];
+    });
 }
-
-
-
-
+-(UIAlertView*)showAlert:(NSString *)title msg:(NSString *)messege {
+    UIAlertView * aa = [[UIAlertView alloc]initWithTitle:title message:messege delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil];
+    [aa show];
+    return aa;
+}
+- (BOOL)validateEmailWithString:(NSString*)checkString
+{
+    BOOL stricterFilter = NO;
+    NSString *stricterFilterString = @"[A-Z0-9a-z\\._%+-]+@([A-Za-z0-9-]+\\.)+[A-Za-z]{2,4}";
+    NSString *laxString = @".+@([A-Za-z0-9-]+\\.)+[A-Za-z]{2}[A-Za-z]*";
+    NSString *emailRegex = stricterFilter ? stricterFilterString : laxString;
+    NSPredicate *emailTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
+    return [emailTest evaluateWithObject:checkString];
+}
 @end
-
-
-
-
-
-
-
-
-
-
