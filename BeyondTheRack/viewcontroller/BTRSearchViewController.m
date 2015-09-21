9eb47dec4751df069d3f104d9807b2130a8e1513
Ali Pourhadi <ali.pourhadi@MTLLMC0629.local>
2015-09-21 12:32:51 -0400

    fixing searchView controller to support iPad size

--- BeyondTheRack/viewcontroller/BTRSearchViewController.m
+++ BeyondTheRack/viewcontroller/BTRSearchViewController.m
@@ -194,7 +194,10 @@
 - (CGSize)collectionView:(UICollectionView *)collectionView
                   layout:(UICollectionViewLayout *)collectionViewLayout
   sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
-    return CGSizeMake(collectionView.frame.size.width / 2 - 1, (collectionView.frame.size.height * 4) / 5);
+    if (collectionView.frame.size.width < 400)
+        return CGSizeMake(collectionView.frame.size.width / 2 - 1, (collectionView.frame.size.height * 4) / 5);
+    else
+        return CGSizeMake(collectionView.frame.size.width / 4 - 4, 500);
 }
 
 - (NSInteger)collectionView:(UICollectionView *)view numberOfItemsInSection:(NSInteger)section {
