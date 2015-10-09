8d1303524df78724f42f20636ba52d19730de8f1
Ali Pourhadi <ali.pourhadi@beyondtherack.com>
2015-10-09 14:38:21 -0400

    changing confirmation page

--- BeyondTheRack/viewcontroller/BTRConfirmationViewController.h
+++ BeyondTheRack/viewcontroller/BTRConfirmationViewController.h
@@ -8,47 +8,11 @@
 
 #import <UIKit/UIKit.h>
 #import "Order.h"
+#import "ConfirmationInfo.h"
 
 @interface BTRConfirmationViewController : UIViewController
 
-// Order
-@property (nonatomic, strong) Order *order;
-
-// ThanksLabel
-@property (weak, nonatomic) IBOutlet UILabel *thanksLabel;
-
-// Billing Information
-@property (weak, nonatomic) IBOutlet UILabel *billingLabel;
-
-// Addresses Outlet
-@property (weak, nonatomic) IBOutlet UILabel *billingAddress;
-@property (weak, nonatomic) IBOutlet UILabel *shippingAddress;
-
-// items of order
-@property (weak, nonatomic) IBOutlet UIView *orderView;
-
-// receipt
-
-@property (weak, nonatomic) IBOutlet UILabel *bagTotalLabel;
-@property (weak, nonatomic) IBOutlet UILabel *shippingPriceLabel;
-
-@property (weak, nonatomic) IBOutlet UILabel *gstTextLabel;
-@property (weak, nonatomic) IBOutlet UILabel *qstTextLabel;
-
-
-@property (weak, nonatomic) IBOutlet UILabel *gstLabel;
-@property (weak, nonatomic) IBOutlet UILabel *qstLabel;
-@property (weak, nonatomic) IBOutlet UILabel *orderTotal;
-
-// Heights
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *viewHeight;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *itemsHeight;
-
-// scrollView
-@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
-
-// paypal
-@property (nonatomic, strong) NSString *transactionID;
-
+// confirmation info
+@property (nonatomic, strong) ConfirmationInfo *info;
 
 @end
--- BeyondTheRack/viewcontroller/BTRConfirmationViewController.m
+++ BeyondTheRack/viewcontroller/BTRConfirmationViewController.m
@@ -11,6 +11,37 @@
 
 @interface BTRConfirmationViewController ()
 
+// ThanksLabel
+@property (weak, nonatomic) IBOutlet UILabel *thanksLabel;
+
+// Billing Information
+@property (weak, nonatomic) IBOutlet UILabel *billingLabel;
+
+// Addresses Outlet
+@property (weak, nonatomic) IBOutlet UILabel *billingAddress;
+@property (weak, nonatomic) IBOutlet UILabel *shippingAddress;
+
+// items of order
+@property (weak, nonatomic) IBOutlet UIView *orderView;
+
+// receipt
+@property (weak, nonatomic) IBOutlet UILabel *bagTotalLabel;
+@property (weak, nonatomic) IBOutlet UILabel *shippingPriceLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *gstTextLabel;
+@property (weak, nonatomic) IBOutlet UILabel *qstTextLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *gstLabel;
+@property (weak, nonatomic) IBOutlet UILabel *qstLabel;
+@property (weak, nonatomic) IBOutlet UILabel *orderTotal;
+
+// Heights
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *viewHeight;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *itemsHeight;
+
+// scrollView
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+
 @end
 
 @implementation BTRConfirmationViewController
@@ -28,65 +59,66 @@
 
 - (void)loadData {
     //changing thanks label
-    NSString *thanksString = [NSString stringWithFormat:@"%@,THANK YOU FOR YOUR ORDER NO. %@",self.order.shippingRecipientName,self.order.orderId];
-    NSRange range = [thanksString rangeOfString:self.self.order.shippingRecipientName];
-    NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc]initWithString:thanksString];
-    [attributedString addAttribute:NSForegroundColorAttributeName value:[UIColor redColor] range:range];
-    self.thanksLabel.attributedText = attributedString;
+    NSString *thanksString = [NSString stringWithFormat:@"%@,THANK YOU FOR YOUR ORDER NO. %@",self.info.billingName,self.info.orderID];
+//    NSRange range = [thanksString rangeOfString:self.info.billingName];
+//    NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc]initWithString:thanksString];
+//    [attributedString addAttribute:NSForegroundColorAttributeName value:[UIColor redColor] range:range];
+//    self.thanksLabel.attributedText = attributedString;
+    self.thanksLabel.text = thanksString;
     
-    if (self.transactionID) {
-        // adding transactionID
-        NSString *string = [NSString stringWithFormat:@"we've billed your order to the transaction number : %@ with following address :",self.transactionID];
-        NSRange range = [string rangeOfString:self.transactionID];
-        NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc]initWithString:string];
-        [attributedString addAttribute:NSForegroundColorAttributeName value:[UIColor redColor] range:range];
-        self.billingLabel.attributedText = attributedString;
-        
-    } else {
-        // adding last 4 digits of card
-        NSMutableAttributedString *text = [[NSMutableAttributedString alloc]initWithAttributedString:self.billingLabel.attributedText];
-        [[text mutableString] replaceOccurrencesOfString:@"1111" withString:[self.order.cardNumber substringWithRange:NSMakeRange(self.order.cardNumber.length - 4, 4)] options:NSCaseInsensitiveSearch range:NSMakeRange(0, text.string.length)];
-        self.billingLabel.attributedText = text;
-    }
+//    if (self.transactionID) {
+//        // adding transactionID
+//        NSString *string = [NSString stringWithFormat:@"we've billed your order to the transaction number : %@ with following address :",self.transactionID];
+//        NSRange range = [string rangeOfString:self.transactionID];
+//        NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc]initWithString:string];
+//        [attributedString addAttribute:NSForegroundColorAttributeName value:[UIColor redColor] range:range];
+//        self.billingLabel.attributedText = attributedString;
+//        
+//    } else {
+//        // adding last 4 digits of card
+//        NSMutableAttributedString *text = [[NSMutableAttributedString alloc]initWithAttributedString:self.billingLabel.attributedText];
+//        [[text mutableString] replaceOccurrencesOfString:@"1111" withString:[self.order.cardNumber substringWithRange:NSMakeRange(self.order.cardNumber.length - 4, 4)] options:NSCaseInsensitiveSearch range:NSMakeRange(0, text.string.length)];
+//        self.billingLabel.attributedText = text;
+//    }
     
     //address
-    NSString* shippingAddressString = [NSString stringWithFormat:@"%@%@\n%@\n%@\n",self.order.shippingAddress.addressLine1,self.order.shippingAddress.addressLine2,self.order.shippingAddress.city,self.order.shippingAddress.country];
+    NSString* shippingAddressString = [NSString stringWithFormat:@"%@%@\n%@\n%@\n",self.info.shippingAddress.addressLine1,self.info.shippingAddress.addressLine2,self.info.shippingAddress.city,self.info.shippingAddress.country];
     self.shippingAddress.text = shippingAddressString;
     
-    NSString* billingAddressString = [NSString stringWithFormat:@"%@%@\n%@\n%@\n",self.order.billingAddress.addressLine1,self.order.billingAddress.addressLine2,self.order.billingAddress.city,self.order.billingAddress.country];
+    NSString* billingAddressString = [NSString stringWithFormat:@"%@%@\n%@\n%@\n",self.info.billingAddress.addressLine1,self.info.billingAddress.addressLine2,self.info.billingAddress.city,self.info.billingAddress.country];
     self.billingAddress.text = billingAddressString;
     
     // order Items
     CGPoint begingingpoint = self.orderView.bounds.origin;
     CGSize size = self.orderView.bounds.size;
     CGFloat space = 140.0;
-    for (int i = 0; i < self.order.items.count; i++) {
-        Item* item = [self.order.items objectAtIndex:i];
+    for (int i = 0; i < self.info.items.count; i++) {
+        Item* item = [self.info.items objectAtIndex:i];
         UILabel *newLabel = [[UILabel alloc]initWithFrame:CGRectMake(begingingpoint.x, begingingpoint.y + (i * space), size.width, space)];
         newLabel.numberOfLines = -1;
         newLabel.font = [UIFont systemFontOfSize:11];
         newLabel.text = [NSString stringWithFormat:@"%@\n%@\n%@",item.shortItemDescription,[BTRAttributeHandler attributeDictionaryToString:item.attributeDictionary],item.brand];
         [self.orderView addSubview:newLabel];
     }
-    self.itemsHeight.constant = self.order.items.count * 140;
-    if (self.order.items.count > 1)
-        self.viewHeight.constant = self.viewHeight.constant + (self.order.items.count - 1) * 140;
+    self.itemsHeight.constant = self.info.items.count * 140;
+    if (self.info.items.count > 1)
+        self.viewHeight.constant = self.viewHeight.constant + (self.info.items.count - 1) * 140;
     
     // Prices
-    self.bagTotalLabel.text = [NSString stringWithFormat:@"%@",self.order.bagTotalPrice];
-    self.orderTotal.text = [NSString stringWithFormat:@"%@",self.order.orderTotalPrice];
-    self.shippingPriceLabel.text = [NSString stringWithFormat:@"%@",self.order.shippingPrice];
+    self.bagTotalLabel.text = [NSString stringWithFormat:@"%@",self.info.bagTotal];
+    self.orderTotal.text = [NSString stringWithFormat:@"%@",self.info.totalOrderValue];
+    self.shippingPriceLabel.text = [NSString stringWithFormat:@"%@",self.info.totalShipping];
     
-    if (self.order.qstTax == nil) {
+    if (self.info.labelTax1 == nil) {
         self.qstLabel.hidden = YES;
         self.qstTextLabel.hidden = YES;
     } else
-        self.qstLabel.text = [NSString stringWithFormat:@"%.2f",self.order.qstTax.floatValue];
-    if (self.order.gstTax == nil) {
+        self.qstLabel.text = self.info.labelTax1; //[NSString stringWithFormat:@"%.2f",self.order.qstTax.floatValue];
+    if (self.info.labelTax2 == nil) {
         self.gstLabel.hidden = YES;
         self.gstTextLabel.hidden = YES;
     } else
-        self.gstLabel.text = [NSString stringWithFormat:@"%.2f",self.order.gstTax.floatValue];
+        self.gstLabel.text = self.info.labelTax2; //[NSString stringWithFormat:@"%.2f",self.order.gstTax.floatValue];
 }
 
 - (IBAction)viewTapped:(id)sender {
