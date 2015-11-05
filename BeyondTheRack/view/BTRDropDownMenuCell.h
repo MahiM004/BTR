5aab008518441e1e52152c4253af4c97d17c9477
Ali Pourhadi <ali.pourhadi@beyondtherack.com>
2015-11-05 17:12:58 -0500

    adding new textfield dropbox

---
+++ BeyondTheRack/view/BTRDropDownMenuCell.h
@@ -0,0 +1,13 @@
+//
+//  BTRDropDownMenuCell.h
+//  BeyondTheRack
+//
+//  Created by Ali Pourhadi on 2015-11-05.
+//  Copyright © 2015 Hadi Kheyruri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BTRDropDownMenuCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *textLabel;
+@end
---
+++ BeyondTheRack/view/BTRDropDownMenuCell.m
@@ -0,0 +1,24 @@
+//
+//  BTRDropDownMenuCell.m
+//  BeyondTheRack
+//
+//  Created by Ali Pourhadi on 2015-11-05.
+//  Copyright © 2015 Hadi Kheyruri. All rights reserved.
+//
+
+#import "BTRDropDownMenuCell.h"
+
+@implementation BTRDropDownMenuCell
+@synthesize textLabel;
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
