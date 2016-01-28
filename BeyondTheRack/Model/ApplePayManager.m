d0dbe209cd6a75aa9c228fc5f0b06c07ebaa8dfa
Ali Pourhadi <ali.pourhadi@beyondtherack.com>
2016-01-28 17:07:15 -0500

    fixing apple pay issues

--- BeyondTheRack/Model/ApplePayManager.m
+++ BeyondTheRack/Model/ApplePayManager.m
@@ -113,13 +113,13 @@
        [summaryItems addObject:[PKPaymentSummaryItem summaryItemWithLabel:@"PROMO CREDIT" amount:[NSDecimalNumber decimalNumberWithString:self.info.promoCredit]]];
     }
     [summaryItems addObject:[PKPaymentSummaryItem summaryItemWithLabel:@"SUBTOTAL" amount:[NSDecimalNumber decimalNumberWithString:self.info.subTotalPrice]]];
-    for (int i = 0; i < [self.info.taxes count]; i++)
-        [summaryItems addObject:[PKPaymentSummaryItem summaryItemWithLabel:[[[self.info.taxes objectAtIndex:i]valueForKey:@"label"]uppercaseString] amount:[NSDecimalNumber decimalNumberWithString:[NSString stringWithFormat:@"%@",[[self.info.taxes firstObject]valueForKey:@"amount"]]]]];
     [summaryItems addObject:[PKPaymentSummaryItem summaryItemWithLabel:@"SHIPPING" amount:[NSDecimalNumber decimalNumberWithString:self.info.shippingPrice]]];
+    for (int i = 0; i < [self.info.taxes count]; i++)
+        [summaryItems addObject:[PKPaymentSummaryItem summaryItemWithLabel:[[[self.info.taxes objectAtIndex:i]valueForKey:@"label"]uppercaseString] amount:[NSDecimalNumber decimalNumberWithString:[NSString stringWithFormat:@"%@",[[self.info.taxes objectAtIndex:i]valueForKey:@"amount"]]]]];
     [summaryItems addObject:[PKPaymentSummaryItem summaryItemWithLabel:@"ORDER TOTAL" amount:[NSDecimalNumber decimalNumberWithString:self.info.orderTotalPrice]]];
     if (self.info.accountCredit.floatValue > 0.0){
         self.info.accountCredit = [NSString stringWithFormat:@"%.2f",self.info.accountCredit.floatValue];
-        [summaryItems addObject:[PKPaymentSummaryItem summaryItemWithLabel:@"PROMO CREDIT" amount:[NSDecimalNumber decimalNumberWithString:self.info.accountCredit]]];
+        [summaryItems addObject:[PKPaymentSummaryItem summaryItemWithLabel:@"ACCOUNT CREDIT" amount:[NSDecimalNumber decimalNumberWithString:self.info.accountCredit]]];
     }
     [summaryItems addObject:[PKPaymentSummaryItem summaryItemWithLabel:@"BEYOND THE RACK" amount:[NSDecimalNumber decimalNumberWithString:self.info.allTotalPrice]]];
     return summaryItems;
